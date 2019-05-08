f947040322ad038d6fc45e4adebc2b88bedf6e2e
Radek Zlebcik <radek.zlebcik@cern.ch>
2019-05-08 11:22:05 +0200

    Blinking led

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,10 +1,17 @@
+int led=9;
+
 void setup ()
 {
   Serial.begin(9600);
+  pinMode(led, OUTPUT);
 }
 
 void loop ()
 {
-    Serial.println("Current Date & Time: ");
+   digitalWrite(led, HIGH); 
+   delay(1000); 
+   digitalWrite(led, LOW); 
+   delay(1000); 
+   Serial.println("Current Date & Time: ");
    delay(1000);
 }
